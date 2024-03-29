ad3c4a8aef160f4f4b89d03be83ab170a4f16ee6
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-03-29 13:16:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const checkBtnDOM = document.querySelector('.check-btn');
+const resultsTipDisplayDOM = document.getElementById('resultTip');
+const resultsDisplayDOM = document.getElementById('result');
+const billInputDOM = document.getElementById('bill-input');
+const rateInputDOM = document.getElementById('rate-input');
+
+ 
+
+checkBtnDOM.addEventListener('click', function () {
+    const bill = billInputDOM.value;
+    const rate = rateInputDOM.value;
+    console.log(bill, rate);
+    let str = "";
+    for (let i = 0; i < rate.length; i++) {
+        if(rate[i] >= "0" && rate[i] <= "9") {
+                str += rate[i];
+        }
+    }
+    
+    const rateNum = +str
+    const billNum = +bill
+    const tip = (rateNum * billNum) / 100;
+    const result = billNum + tip;
+
+    resultsTipDisplayDOM.innerHTML = `${tip}`;
+    resultsDisplayDOM.innerHTML = `${result}`;
+
+ 
+});
+ 
